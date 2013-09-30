ac3004c2c71ac3f99b6bb43aa57a419b4e1090e0
Michael John Decker <mdecker6@kent.edu>
2013-09-30 17:12:39 -0400

    Adding Code for sax interface

---
+++ SAX2srcMLHandler.hpp
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_SAX2SRCMLHANDLER_HPP
+#define INCLUDED_SAX2SRCMLHANDLER_HPP
+
+xmlSAXHandler factory();
+
+void startDocument(void * ctx);
+
+void endDocument(void * ctx);
+void startElementNs(void * ctx, const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
+                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                           const xmlChar ** attributes);
+
+void endElementNs(void * ctx, const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI);
+
+void characters(void * ctx, const xmlChar * ch, int len);
+
+#endif
