9237995ade93546dcc6fef43fc9ce71d506edb77
Michael John Decker <mdecker6@kent.edu>
2013-09-30 17:27:59 -0400

    Correcting to not pass context

--- src/srcMLHandler.hpp
+++ src/srcMLHandler.hpp
@@ -3,17 +3,17 @@
 
 class srcMLHandler {
 
-  virtual void startDocument(void * ctx) {}
+  virtual void startDocument() {}
 
-  virtual void endDocument(void * ctx) {}
+  virtual void endDocument() {}
 
-  virtual void startElementNs(void * ctx, const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
+  virtual void startElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
                            int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
                       const xmlChar ** attributes) {}
 
-  virtual void endElementNs(void * ctx, const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {}
+  virtual void endElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {}
 
-  virtual void characters(void * ctx, const xmlChar * ch, int len) {}
+  virtual void characters(const xmlChar * ch, int len) {}
 
 }
 
