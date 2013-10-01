083ca68c6fbe2d47675399c424e7d7072e8ad753
Michael John Decker <mdecker6@kent.edu>
2013-10-01 10:20:05 -0400

    Adding tests for threading

---
+++ examples/srcMLHandlerThread.hpp
@@ -0,0 +1,75 @@
+#ifndef INCLUDED_SRCMLHANDLEREXAMPLE_HPP
+#define INCLUDED_SRCMLHANDLEREXAMPLE_HPP
+
+#include <srcMLHandler.hpp>
+
+#include <libxml/parser.h>
+#include <stdio.h>
+
+class srcMLHandlerExample : public srcMLHandler {
+
+public :
+
+  virtual void startDocument() {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    
+  }
+
+  virtual void endDocument() {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void startRoot(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
+                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                      const xmlChar ** attributes) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void startUnit(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
+                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                      const xmlChar ** attributes) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void startElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
+                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                      const xmlChar ** attributes) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void endRoot(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void endUnit(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void endElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+  virtual void characters(const xmlChar * ch, int len) {
+
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+  }
+
+};
+
+#endif
---
+++ examples/thread.cpp
@@ -0,0 +1,26 @@
+#include <srcMLHandlerExample.hpp>
+#include <SAX2srcMLHandler.hpp>
+#include <srcMLControlHandler.hpp>
+
+#include <string>
+#include <libxml/parserInternals.h>
+
+void * start_routine(void *) {
+
+  
+  srcMLControlHandler control("test.xml");
+  srcMLHandlerThread handler;
+  control.parse(&handler);
+
+  return 0;
+
+}
+
+int main(int argc, char * argv[]) {
+
+
+  pthread_t thread;
+  pthread_create(&thread, 0, start_routine, &arg);
+
+  return 0;
+}
