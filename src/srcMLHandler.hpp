93b3a1cb5f5e41bf24e14ae82d854307f782d8e5
Michael John Decker <mdecker6@kent.edu>
2013-09-30 21:26:26 -0400

    More documentation

--- src/srcMLHandler.hpp
+++ src/srcMLHandler.hpp
@@ -31,31 +31,62 @@ public :
 
   srcMLHandler() {}
 
+  /**
+   * startDocument
+   * @param ctx an xmlParserCtxtPtr
+   *
+   * SAX handler function for start of document.
+   * Overide for desired behaviour.
+   */
   virtual void startDocument() {}
 
+  /**
+   * endDocument
+   * @param ctx an xmlParserCtxtPtr
+   *
+   * SAX handler function for end of document.
+   * Overide for desired behaviour.
+   */
   virtual void endDocument() {}
 
-/**
- * startElementNs
- * @param ctx an xmlParserCtxtPtr
- * @param localname the name of the element tag
- * @param prefix the tag prefix
- * @param URI the namespace of tag
- * @param nb_namespaces number of namespaces definitions
- * @param namespaces the defined namespaces
- * @param nb_attributes the number of attributes on the tag
- * @param nb_defaulted the number of defaulted attributes
- * @param attributes list of attribute name value pairs (localname/prefix/URI/value/end)
- *
- * SAX handler function for start of an element.
- * Immediately calls supplied handlers function.
- */
+  /**
+   * startElementNs
+   * @param ctx an xmlParserCtxtPtr
+   * @param localname the name of the element tag
+   * @param prefix the tag prefix
+   * @param URI the namespace of tag
+   * @param nb_namespaces number of namespaces definitions
+   * @param namespaces the defined namespaces
+   * @param nb_attributes the number of attributes on the tag
+   * @param nb_defaulted the number of defaulted attributes
+   * @param attributes list of attribute name value pairs (localname/prefix/URI/value/end)
+   *
+   * SAX handler function for start of an element.
+   * Overide for desired behaviour.
+   */
   virtual void startElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
-                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
-                      const xmlChar ** attributes) {}
+                              int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                              const xmlChar ** attributes) {}
 
+  /**
+   * endElementNs
+   * @param ctx an xmlParserCtxtPtr
+   * @param localname the name of the element tag
+   * @param prefix the tag prefix
+   * @param URI the namespace of tag
+   *
+   * SAX handler function for end of an element.
+   * Overide for desired behaviour.
+   */
   virtual void endElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {}
 
+  /**
+   * characers
+   * @param ctx an xmlParserCtxtPtr
+   *
+   * SAX handler function for character handling.
+   * Overide for desired behaviour.
+   */
   virtual void characters(const xmlChar * ch, int len) {}
 
 };
