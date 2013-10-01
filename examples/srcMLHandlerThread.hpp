1829342eb1844a844ad0852914335f500af104a2
Michael John Decker <mdecker6@kent.edu>
2013-10-01 10:47:35 -0400

    Trying to check if done

--- examples/srcMLHandlerThread.hpp
+++ examples/srcMLHandlerThread.hpp
@@ -12,20 +12,34 @@ class srcMLHandlerThread : public srcMLHandler {
 private :
 
   pthread_mutex_t mutex;
+  pthread_mutex_t is_done_mutex;
   pthread_cond_t cond;
+
+  bool is_done;
 public :
 
-  srcMLHandlerThread() {
-    
+  srcMLHandlerThread() : is_done(false) {
+
     pthread_mutex_init(&mutex, 0);
+    pthread_mutex_init(&is_done_mutex, 0);
     pthread_cond_init(&cond, 0);
   }
 
   void wait() {
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    while(true) {
+      pthread_mutex_lock(&is_done_mutex);
+      if(is_done) {
 
-  while(pthread_mutex_trylock(&mutex) == 0)
-    pthread_mutex_unlock(&mutex);
+        is_done = false;
+        pthread_mutex_unlock(&mutex);
+        break;
 
+      }
+      pthread_mutex_unlock(&mutex);
+
+    }
+    fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
   }
 
   void resume() {
@@ -35,7 +49,7 @@ public :
   virtual void startDocument() {
 
     fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-    
+
   }
 
   virtual void endDocument() {
@@ -45,24 +59,24 @@ public :
   }
 
   virtual void startRoot(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
-                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
-                      const xmlChar ** attributes) {
+                         int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                         const xmlChar ** attributes) {
 
     fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
 
   }
 
   virtual void startUnit(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
-                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
-                      const xmlChar ** attributes) {
+                         int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                         const xmlChar ** attributes) {
 
     fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
 
   }
 
   virtual void startElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI,
-                           int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
-                      const xmlChar ** attributes) {
+                              int nb_namespaces, const xmlChar ** namespaces, int nb_attributes, int nb_defaulted,
+                              const xmlChar ** attributes) {
 
     fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
 
@@ -79,6 +93,10 @@ public :
     fprintf(stderr, "HERE: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
     // pause
     pthread_cond_wait(&cond, &mutex);
+    pthread_mutex_lock(&is_done_mutex);
+    is_done = true;
+    pthread_mutex_unlock(&mutex);
+
   }
 
   virtual void endElementNs(const xmlChar * localname, const xmlChar * prefix, const xmlChar * URI) {
