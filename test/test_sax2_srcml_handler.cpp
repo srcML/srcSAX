4e1779d6b64548a98656465c9c09bf5f3a7bda65
Michael John Decker <mdecker6@kent.edu>
2013-10-03 18:56:34 -0400

    Testing factory

---
+++ test/test_sax2_srcml_handler.cpp
@@ -0,0 +1,28 @@
+#include <SAX2srcMLHandler.hpp>
+
+#include <stdio.h>
+#include <string.h>
+#include <cassert>
+
+int main(int argc, char * argv[]) {
+
+  /*
+    factory
+   */
+
+  {
+
+    xmlSAXHandler sax = factory();
+    assert(sax.initialized == XML_SAX2_MAGIC);
+    assert(sax.startDocument == startDocument);
+    assert(sax.endDocument == endDocument);
+    assert(sax.startElementNs == startRoot);
+    assert(sax.endElementNs == endElementNs);
+    assert(sax.characters == charactersFirst);
+    assert(sax.comment == comment);
+    assert(sax.cdataBlock == cdataBlock);
+
+  }
+
+  return 0;
+}
