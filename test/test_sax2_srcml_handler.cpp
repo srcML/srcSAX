7e5dc37ff2a9004a2b7be6e0301e0ebfa42fbcf7
Michael John Decker <mdecker6@kent.edu>
2013-10-03 19:30:20 -0400

    Testing startRoot

--- test/test_sax2_srcml_handler.cpp
+++ test/test_sax2_srcml_handler.cpp
@@ -39,5 +39,42 @@ int main(int argc, char * argv[]) {
 
   }
 
+  /*
+    endDocument
+   */
+  {
+
+    srcMLHandler handler;
+    SAX2srcMLHandler sax2_handler;
+    sax2_handler.process = &handler;
+
+    xmlParserCtxt ctxt;
+    ctxt._private = &sax2_handler;
+    endDocument(&ctxt);
+
+  }
+
+  /*
+    startRoot
+   */
+  {
+
+    srcMLHandler handler;
+    SAX2srcMLHandler sax2_handler;
+    sax2_handler.process = &handler;
+
+    xmlParserCtxt ctxt;
+    ctxt._private = &sax2_handler;
+    const char * namespaces[4] = { "src", "http://www.sdml.info/srcML/src", "cpp", "http://www.sdml.info/srcML/cpp" };
+    const char * values = "abc";
+    const char * attributes[15] = { "filename", "src", "http://www.sdml.info/srcML/src", values, values + 1,
+                                    "dir", "src", "http://www.sdml.info/srcML/src", values + 1, values + 2,
+                                   "language", "src", "http://www.sdml.info/srcML/src", values + 2, values + 3 };
+    startRoot(&ctxt, (const xmlChar *)"unit", (const xmlChar *)"src", 
+              (const xmlChar *)"http://www.sdml.info/srcML/src", 2, (const xmlChar **)namespaces, 3, 0,
+              (const xmlChar **) attributes);
+
+  }
+
   return 0;
 }
