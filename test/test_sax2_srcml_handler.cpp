e7ae8dc524b3316c9bb005966a7e87ccf9602fe5
Michael John Decker <mdecker6@kent.edu>
2013-10-03 19:41:23 -0400

    More testing

--- test/test_sax2_srcml_handler.cpp
+++ test/test_sax2_srcml_handler.cpp
@@ -74,6 +74,32 @@ int main(int argc, char * argv[]) {
               (const xmlChar *)"http://www.sdml.info/srcML/src", 2, (const xmlChar **)namespaces, 3, 0,
               (const xmlChar **) attributes);
 
+    assert((const char *)sax2_handler.root.localname == std::string("unit"));
+    assert((const char *)sax2_handler.root.prefix == std::string("src"));
+    assert((const char *)sax2_handler.root.URI == std::string("http://www.sdml.info/srcML/src"));
+    assert(sax2_handler.root.nb_namespaces == 2);
+    assert((const char *)sax2_handler.root.namespaces[0] == std::string("src"));
+    assert((const char *)sax2_handler.root.namespaces[1] == std::string("http://www.sdml.info/srcML/src"));
+    assert((const char *)sax2_handler.root.namespaces[2] == std::string("cpp"));
+    assert((const char *)sax2_handler.root.namespaces[3] == std::string("http://www.sdml.info/srcML/cpp"));
+    assert(sax2_handler.root.nb_attributes == 3);
+    assert(sax2_handler.root.nb_defaulted == 0);
+    assert((const char *)sax2_handler.root.attributes[0] == std::string("filename"));
+    assert((const char *)sax2_handler.root.attributes[1] == std::string("src"));
+    assert((const char *)sax2_handler.root.attributes[2] == std::string("http://www.sdml.info/srcML/src"));
+    assert(const char *)sax2_handler.root.attributes[3] == values);
+    assert(const char *)sax2_handler.root.attributes[4] == values + 1);
+    assert((const char *)sax2_handler.root.attributes[5] == std::string("dir"));
+    assert((const char *)sax2_handler.root.attributes[6] == std::string("src"));
+    assert((const char *)sax2_handler.root.attributes[7] == std::string("http://www.sdml.info/srcML/src"));
+    assert(const char *)sax2_handler.root.attributes[8] == values + 1);
+    assert(const char *)sax2_handler.root.attributes[9] == values + 2);
+    assert((const char *)sax2_handler.root.attributes[10] == std::string("language"));
+    assert((const char *)sax2_handler.root.attributes[11] == std::string("src"));
+    assert((const char *)sax2_handler.root.attributes[12] == std::string("http://www.sdml.info/srcML/src"));
+    assert(const char *)sax2_handler.root.attributes[13] == values + 2);
+    assert(const char *)sax2_handler.root.attributes[14] == values + 3);
+
   }
 
   return 0;
