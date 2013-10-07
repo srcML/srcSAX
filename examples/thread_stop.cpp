af67dee0ea210aa77cbf1681436cf3b2f05850fb
Michael John Decker <mdecker6@kent.edu>
2013-10-07 19:32:19 -0400

    Adding thread stop item

---
+++ examples/thread_stop.cpp
@@ -0,0 +1,39 @@
+#include <srcMLHandlerThread.hpp>
+#include <SAX2srcMLHandler.hpp>
+#include <srcMLControlHandler.hpp>
+
+#include <string>
+#include <libxml/parserInternals.h>
+
+#include <pthread.h>
+
+void * start_routine(void * arg) {
+
+  srcMLHandlerThread * handler = (srcMLHandlerThread *)arg;
+
+  srcMLControlHandler control("thread.xml");
+  control.parse(handler);
+
+  return 0;
+
+}
+
+int main(int argc, char * argv[]) {
+
+
+  pthread_t thread;
+  srcMLHandlerThread arg;
+  pthread_create(&thread, 0, start_routine, &arg);
+
+
+  arg.wait();
+  arg.resume();
+
+  arg.wait();
+  arg.resume();
+
+  void * ret;
+  pthread_join(thread, &ret);
+
+  return 0;
+}
