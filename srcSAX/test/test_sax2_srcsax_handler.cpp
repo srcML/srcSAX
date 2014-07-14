967bccc52bc855e1aa0e420be8db0f7653d44f73
Michael John Decker <mdecker6@kent.edu>
2014-07-14 15:40:32 -0400

    Getting more of examples setup and turn off testing for now.

--- srcSAX/test/test_sax2_srcsax_handler.cpp
+++ srcSAX/test/test_sax2_srcsax_handler.cpp
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <SAX2srcSAXHandler.hpp>
+#include <sax2_srcsax_handler.hpp>
 #include <srcSAXHandler.hpp>
 
 #include <stdio.h>
