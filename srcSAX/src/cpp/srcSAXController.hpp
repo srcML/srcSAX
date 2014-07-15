11ead55cd8795d3bd9af6f8292350dc69cc770cb
Michael John Decker <mdecker6@kent.edu>
2014-07-15 09:50:47 -0400

    More copyright updates.

--- srcSAX/src/cpp/srcSAXController.hpp
+++ srcSAX/src/cpp/srcSAXController.hpp
@@ -3,12 +3,12 @@
  *
  * @copyright Copyright (C) 2013-2014 SDML (www.srcML.org)
  *
- * The srcML Toolkit is free software; you can redistribute it and/or modify
+ * srcSAX is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * The srcML Toolkit is distributed in the hope that it will be useful,
+ * srcSAX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
